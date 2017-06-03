cd30f26d29cdf562d03ee9761cfcc82ddb7f1c8f
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-06-03 18:57:45 +0900

    testifying

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import ImageView from './component/imageView';
+import axios from 'axios';
+// Read the file into memory.
+
+const URL = 'temp';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.getImage = this.getImage.bind(this);
+  }
+  getImage() {
+    axios.get(URL)
+    .then(result => {
+      console.log(result);
+    })
 
+  }
+  render() {
+    this.getImage();
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to smartCloset</h2>
+            <button onClick={this.sendImage}>
+              Activate Lasers
+            </button>
           <img src={logo} className="App-logo" />
         </div>
         <div>
